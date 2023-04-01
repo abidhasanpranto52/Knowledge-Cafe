da2655cb03c69c30cfce8121c7485a96501c1f16
abidhasanpranto52 <abidhasanpranto52@gmail.com>
2023-04-01 19:35:12 +0600

    recheck project

--- src/components/SideCart/SideCart.jsx
+++ src/components/SideCart/SideCart.jsx
@@ -8,10 +8,10 @@ const SideCart = ({ cart, book }) => {
   }
   return (
     <div className="sticky top-0">
-      <div className="p-5 w-80 lg:w-96  border-violet-600 border rounded-lg font-semibold text-xl text-violet-800 bg-violet-100">
+      <div className="p-5 w-80 lg:w-96  border-violet-600 border rounded-lg font-semibold text-xl text-violet-800 bg-red-100">
         <h1>Spent time on read: {countTime}</h1>
       </div>
-      <div className="bg-slate-200 w-80 bg-opacity-70 p-5 mt-5 rounded-lg lg:w-96 space-y-2">
+      <div className="bg-violet-300 w-80 bg-opacity-70 p-5 mt-5 rounded-lg lg:w-96 space-y-2">
         <h1 className="font-semibold text-slate-900 text-lg">
           Bookmarked Blogs: {book.length}
         </h1>
