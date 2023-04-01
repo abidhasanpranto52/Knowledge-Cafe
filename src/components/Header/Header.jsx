7f6cce042769c936240fe180ab584019b7a3f706
abidhasanpranto52 <abidhasanpranto52@gmail.com>
2023-04-01 19:30:10 +0600

    change header image

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
         <>
         <div  className='max-w-5xl mx-auto flex justify-between items-center px-3 pt-6 pb-4'>
             <h1 className='lg:text-3xl text-2xl font-bold'>Knowledge <span className='text-orange-500'>Cafe</span></h1>
-            <img className='w-16 rounded-[50%]' src="https://img.freepik.com/free-vector/farmer-using-agricultural-technology_53876-120543.jpg?w=740&t=st=1680200650~exp=1680201250~hmac=df60315276e538e159a253b774c219e3e6cf5e074df60363e1b0bce3639fdd36" alt="" />
+            <img className='w-16 rounded-[50%]' src="https://img.freepik.com/free-photo/hooded-computer-hacker-stealing-information-with-laptop_155003-1918.jpg?w=1380&t=st=1680355540~exp=1680356140~hmac=88b708ef33bbe177180372617ca86e539b3070079404cad0ef321501ab0c03ba" alt="" />
            
         </div>
         <hr className=' border'/>
