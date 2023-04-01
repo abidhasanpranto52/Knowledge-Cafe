8d2263d4d79be803625941f22b55ab641bfe1f56
abidhasanpranto52 <abidhasanpranto52@gmail.com>
2023-04-01 19:13:00 +0600

    tailwindcss install

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Card from './components/Card/Card';
+import Header from './components/Header/Header';
+import Questions from './components/Questions/Questions';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='max-w-6xl mx-auto '>
+      <Header></Header>
+      <Card></Card>
+      <Questions></Questions>
     </div>
+    </>
   );
-}
+};
 
 export default App;
